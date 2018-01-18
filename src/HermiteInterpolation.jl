54407859d60fb90a46784311ab8ead8c7f07f275
chriselrod <elrodc@gmail.com>
2018-01-18 08:06:10 -0600

    Initial commit.

--- src/HermiteInterpolation.jl
+++ src/HermiteInterpolation.jl
@@ -1,5 +1,22 @@
+__precompile__()
+
 module HermiteInterpolation
 
-# package code goes here
+using Reduce, SpecialFunctions, Base.Cartesian, Sobol
+
+include("sobol.jl")
+include("polynomial/polynomials.jl")
+include("polynomial/solution_matrix.jl")
+
+export  sobol_vec,
+        max_degree_poly,
+        lnorm_poly,
+        create_linear_map,
+        linear_map_stable
+
+function __init__()
+    Reduce.Rational(false)
+
+end
 
 end # module
