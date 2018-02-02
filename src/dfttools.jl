b0d4a94e49bab611bb3ed8cc68f53c842d544b3f
chriselrod <elrodc@gmail.com>
2018-02-02 17:39:54 -0600

    DFT updates.

--- src/dfttools.jl
+++ src/dfttools.jl
@@ -17,39 +17,20 @@ end
 ### This approach probably does not scale well to larger problems.
 
 
+ωr(N, i) = cospi(-2(i % N)/N)
+ωi(N, i) = sinpi(-2(i % N)/N)
+ω(N, i) = ωr(N, i) + im*ωi(N, i)
+
 ωr( ::Val{N}, ::Val{0} ) where N = 1.0
-@generated function ωr( ::Val{N}, ::Val{i} ) where {N,i}
-    r = i % N
-    ###This is mostly an aesthetic choice, so that cosine evaluates to exactly zero when it should, instead of ≈ eps().
-    if abs(4r) == N || abs(4r) == 3N 
-        out = 0.0
-    elseif abs(2r) == N
-        out = -1.0
-    elseif r == 0
-        out = 1.0
-    else
-        out = cos(-2π*r/N)
-    end
-    out
-end
+@generated ωr( ::Val{N}, ::Val{i} ) where {N,i} = ωr(N, i)
 ωi( ::Val{N}, ::Val{0} ) where N = 0.0
-@generated function ωi( ::Val{N}, ::Val{i} ) where {N,i}
-    r = i % N
-    if 4r == N || 4r == -3N
-        out = -1.0
-    elseif 4r == 3N || 4r == -N
-        out = 1.0
-    elseif abs(2r) == N
-        out = 0.0
-    else
-        out = sin(-2π*r/N)
-    end
-    out
-end
+@generated ωi( ::Val{N}, ::Val{i} ) where {N,i} = ωi(N, i)
 @generated ωr( ::Val{N}, ::Val{i} ) where {N,i} = cos(-2π*i/N)
 @generated ωi( ::Val{N}, ::Val{i} ) where {N,i} = sin(-2π*i/N)
 ω( ::Val{N}, ::Val{0} ) where N = 1.0 + 0.0im 
 @generated ω( ::Val{N}, ::Val{i} ) where {N,i} = ωr(Val{N}(), Val{i}()) + im*ωi(Val{N}(), Val{i}())
+
+
 
 
 function dft_sum_expr(N, ::Type{T} = Float64) where T
@@ -69,10 +50,8 @@ function dft_even_odd_split_expr(N)
     e_arg = :( (x[1], ) )
     o_arg = :( (x[2], ) )
     for i ∈ 2:d
-        inde = 2i-1
-        indo = 2i
-        push!(e_arg.args, :(x[$inde]))
-        push!(o_arg.args, :(x[$indo]))
+        push!(e_arg.args, :(x[$(2i-1)]))
+        push!(o_arg.args, :(x[$(2i)]))
     end
     e_arg, o_arg
 end
@@ -91,8 +70,69 @@ end
 end
 
 
-@inline function dft(x::NTuple{2,T}) where N
-    ωr(Val{1}())
+function recursion_level(d_array, recursion_levels)
+    end_recursion_size = d_array[recursion_levels + 1] #Smallest component in which we can break this down.
+
+    step = 1 << recursion_levels
+    initial_eval = quote end
+    for i ∈ 1:step
+        base_step!(initial_eval.args, end_recursion_size, i, step, d_array[1])
+    end
+    initial_eval
+end
+
+function base_step(blength, offset, step, N, x = :x_, r = :r_)
+    base = :( $(Symbol(r, offset)) = $(Symbol(x, offset)) + $(Symbol(x, offset + step)) * $(ω(N, step)))
+    for i ∈ 2:blength-1
+        push!(base.args[2].args, :( $(Symbol(x, offset + i*step)) * $(ω(N, i*step)) ))
+    end
+    base
+end
+
+function base_step!(initial, blength, offset, step, N, x = :x_, r = :r_)
+    for j ∈ 1:blength        
+        push!(initial.args, base_step(blength, offset, step, N, x, r))
+    end
+    initial
+end
+
+function compose( level, step )
+
+
+end
+
+
+qe = quote
+        x = a + b
+        y = c * d
+        z = x * y + 4
+    end
+
+function recursion_meta(n)
+    recursion_levels = 0
+    d, r = divrem(n, 2)
+    d_array = [n,d]
+    while r != 1
+        recursion_levels += 1
+        d, r = divrem(d, 2)
+        push!(d_array, d)
+        d == 1 && break
+    end
+    d_array, recursion_levels
+end
+
+@generated function dfte(x::Ntuple{n,T}) where {n,T}
+
+    quote
+        @nextract $n x x
+
+    end
+end
+
+@nexprs $N i -> begin
+    @nexprs $P-i j -> begin
+        i * j
+    end
 end
 
 
@@ -111,3 +151,30 @@ end
 @inline function dft(x::NTuple{2,T}, ::Val{N} = Val{2}()) where {T,N}
     ωr(Val{1}())
 end
+
+
+
+
+
+function dft_call_expr(N)
+    d = N ÷ 2
+    e_arg = :( dftva(x[1]) )
+    o_arg = :( dftva(x[2]) )
+    for i ∈ 2:d
+        push!(e_arg.args, :(x[$(2i-1)]))
+        push!(o_arg.args, :(x[$(2i)]))
+    end
+    e_arg, o_arg
+end
+@inline dftva(x::T) where T <: Number = x
+@generated function dftva(x::Vararg{T,n}) where {n,T}
+    out = dft_sum_expr(n, T)
+    e_arg, o_arg = dft_call_expr(n)
+    quote
+        $(Expr(:meta, :inline))
+        ffte = $e_arg
+        ffto = $o_arg
+        $out
+    end
+end
+
