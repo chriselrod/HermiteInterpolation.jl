8620d3d366c1e69b3ab4719894ffc9c00893780b
chriselrod <elrodc@gmail.com>
2018-03-14 15:56:17 -0500

    Updates; got Optim working correctly on the NoGoldStandards problem.

--- test/NoGoldStandard.jl
+++ test/NoGoldStandard.jl
@@ -4,7 +4,7 @@
 # Distributions does not work yet on Julia 0.7
 beta_lpdf(x, alpha, beta) = (alpha-1)*log(x) + (beta-1)*log(1-x)
 
-using StaticArrays
+using StaticArrays, Base.Cartesian
 const TypedLength{N,T} = Union{NTuple{N,T}, StaticVector{N,T}}
 
 function generate_call(fname, n)
@@ -14,13 +14,75 @@ function generate_call(fname, n)
     end
     out
 end
+
+
+
+# function generate_call_transform(fname, n)
+#     verind = VERSION > v"0.6.9" ? 3 : 2
+#     out = quote
+#         @inbounds target = $fname( data, invhalflogit(p_1) )
+#         @inbounds target = target + logdinvlogit(p_1)
+#         @inbounds target = target + logdinvlogit(p_5)#/2
+#         target
+#     end
+#     for i ∈ 2:4
+#         p_i = Symbol(:p_, i)
+#         push!(out.args[2].args[verind].args[2].args, :(invhalflogit($p_i)))
+#         push!(out.args[4].args[verind].args[2].args, :(logdinvlogit($p_i)))
+#     end
+#     push!(out.args[2].args[2].args[2].args, :(invlogit(p_5)))
+#     for i ∈ 6:n
+#         p_i = Symbol(:p_, i)
+#         push!(out.args[2].args[verind].args[2].args, :(invlogit($p_i)))
+#         push!(out.args[6].args[verind].args[2].args, :(logdinvlogit($p_i)))#/2))
+#     end
+#     out
+# end
+
+function generate_call_transform(fname, n)
+    verind = VERSION > v"0.6.9" ? 3 : 2
+    out = quote
+        @inbounds target = $fname( data, (p_1+1)/2 )
+        @inbounds target += log(p_1) + log(1 - p_1)
+        @inbounds target += log(p_5) + log(1 - p_5)
+        target
+    end
+    for i ∈ 2:4
+        p_i = Symbol(:p_, i)
+        push!(out.args[2].args[verind].args[2].args, :( ($p_i+1)/2 ))
+        push!(out.args[4].args[verind].args[2].args, :( log($p_i) + log(1-$p_i)  ))
+    end
+    push!(out.args[2].args[2].args[2].args, :(p_5))
+    for i ∈ 6:n
+        p_i = Symbol(:p_, i)
+        push!(out.args[2].args[verind].args[2].args, :($p_i))
+        push!(out.args[6].args[verind].args[2].args, :( log($p_i) + log(1-$p_i)  ))
+    end
+    out
+end
+
+
 # Expr(:meta, :inline)
 @generated function NGS(data::Tuple, params::TypedLength{N,T}) where {N,T}
-    generate_call(:NoGoldStandard, N )
+    generate_call_transform(:NoGoldStandard, N )
+end
+@generated function NGS(data::Tuple, params::AbstractVector{T}, ::Val{N}) where {N,T}
+    call = generate_call_transform(:NoGoldStandard, N )
+#    Nm4 = N - 4
+    quote
+        @nexprs $N i -> begin
+            p_i = invlogit(params[i])
+        end
+        # @nexprs $Nm4 i -> begin
+        #     p_{i+4} = invlogit(params[i+4])
+        # end
+        $call
+        -target
+    end
 end
 
 """Log Density of the No Gold-Standard Model"""
-function NoGoldStandard(priordata::Tuple, S₁::T, S₂::T, C₁::T, C₂::T, pi::Vararg{T,K}) where {T,K}
+@inline function NoGoldStandard(priordata::Tuple, S₁::T, S₂::T, C₁::T, C₂::T, pi::Vararg{T,K}) where {T,K}
     n, alpha_pi, beta_pi, alpha_S1, beta_S1, alpha_S2, beta_S2,
         alpha_C1, beta_C1, alpha_C2, beta_C2 = priordata
     target = beta_lpdf(S₁, alpha_S1, beta_S1) + beta_lpdf(S₂, alpha_S2, beta_S2)
@@ -33,27 +95,28 @@ function NoGoldStandard(priordata::Tuple, S₁::T, S₂::T, C₁::T, C₂::T, pi
     @inbounds for k in 1:K
         target += beta_lpdf( pi[k], alpha_pi, beta_pi )
 
-        target += n[k,1] * log( pi[k] * (   S₁ *   S₂ ) + (1-pi[k]) * ((1-C₁)*(1-C₂)) )
-        target += n[k,2] * log( pi[k] * (   S₁ *(1-S₂)) + (1-pi[k]) * ((1-C₁)*   C₂ ) )
-        target += n[k,3] * log( pi[k] * ((1-S₁)*   S₂ ) + (1-pi[k]) * (   C₁ *(1-C₂)) )
-        target += n[k,4] * log( pi[k] * ((1-S₁)*(1-S₂)) + (1-pi[k]) * (   C₁ *   C₂ ) )
+        target += n[1,k] * log( pi[k] * (   S₁ *   S₂ ) + (1-pi[k]) * ((1-C₁)*(1-C₂)) )
+        target += n[2,k] * log( pi[k] * (   S₁ *(1-S₂)) + (1-pi[k]) * ((1-C₁)*   C₂ ) )
+        target += n[3,k] * log( pi[k] * ((1-S₁)*   S₂ ) + (1-pi[k]) * (   C₁ *(1-C₂)) )
+        target += n[4,k] * log( pi[k] * ((1-S₁)*(1-S₂)) + (1-pi[k]) * (   C₁ *   C₂ ) )
 
-        target += n[k,5] * log( pi[k]*   S₁  + (1-pi[k])*(1-C₁) )
-        target += n[k,6] * log( pi[k]*(1-S₁) + (1-pi[k])*   C₁  )
+        target += n[5,k] * log( pi[k]*   S₁  + (1-pi[k])*(1-C₁) )
+        target += n[6,k] * log( pi[k]*(1-S₁) + (1-pi[k])*   C₁  )
 
-        target += n[k,7] * log( pi[k]*   S₂  + (1-pi[k])*(1-C₂) )
-        target += n[k,8] * log( pi[k]*(1-S₂) + (1-pi[k])*   C₂  )
+        target += n[7,k] * log( pi[k]*   S₂  + (1-pi[k])*(1-C₂) )
+        target += n[8,k] * log( pi[k]*(1-S₂) + (1-pi[k])*   C₂  )
     end
     target
 end
 
-logit(x) = log(x/(1-x))
-invlogit(x) = 1/(1+exp(-x))
-function dinvlogit(x)
+@inline logit(x) = log(x/(1-x))
+@inline invlogit(x) = 1/(1+exp(-x))
+@inline invhalflogit(x) = (invlogit(x)+1)/2
+@inline function dinvlogit(x)
     expnx = exp(-x)
     expnx / abs2(1+expnx)
 end
-
+@inline logdinvlogit(x) = -x - 2log(1+exp(-x))
 
 # using LineSearches, Optim, BenchmarkTools
 # rosenbrock(x) =  (1.0 - x[1])^2 + 100.0 * (x[2] - x[1]^2)^2
@@ -165,10 +228,58 @@ n_big_4    = gen_data(corr_errors_independent4, 1600, 3200, 252);
 
 
 
-const cds2 = (n_small_4, 1,1,1,1,1,1,1,1,1,1);
+const cds4 = (n_small_4,1,1,1,1,1,1,1,1,1,1);
+const cdb4 = (n_big_4,1,1,1,1,1,1,1,1,1,1);
+using Optim, LineSearches
+
+initial_x = zeros(8);
+NGS(cds2, initial_x, Val{8}())
+hage = BFGS()
+back = BFGS(; linesearch = LineSearches.BackTracking())
+ntr = NewtonTrustRegion()
+optimize(x -> NGS(cdb4, x, Val{8}()), initial_x, hage)
+optimize(x -> NGS(cdb4, x, Val{8}()), initial_x, back)
+optimize(x -> NGS(cdb4, x, Val{8}()), initial_x, ntr)
+
+using BenchmarkTools
+@benchmark optimize(x -> NGS(cdb4, x, Val{8}()), $initial_x, $hage)
+@benchmark optimize(x -> NGS(cdb4, x, Val{8}()), $initial_x, $back)
+@benchmark optimize(x -> NGS(cdb4, x, Val{8}()), $initial_x, $ntr)
+
+#Backtrack fastest, in part because it doesn't need nearly as many gradient calls.
+
+
+detransform_res(x::Optim.MultivariateOptimizationResults) = detransform_res(Optim.minimizer(x))
+function detransform_res(x)
+    out = similar(x)
+    for i ∈ 1:4
+        out[i] = (1+invlogit(x[i]))/2
+    end
+    for i ∈ 5:length(x)
+        out[i] = invlogit(x[i])
+    end
+    out
+end
+
+
+function print_mat_for_copy(m)
+    out = "["
+    for j ∈ 1:size(m,2)
+        out *= string(m[1,j]) * " "
+    end
+    for i ∈ 2:size(m,1)
+        out *= ";\n"
+        for j ∈ 1:size(m,2)
+            out *= string(m[i,j]) * " "
+        end
+    end
+    out * "]"
+end
+
+const cds2 = (n_small_4,1,1,1,1,1,1,1,1,1,1);
 x = ntuple(i -> rand(), Val(8));
 using BenchmarkTools
-@benchmark NGS($ds2, $x)
+@benchmark NGS(cds2, $x)
 
 
 # julia> ForwardDiff.gradient(x -> NGS(cds2,x), svx)
